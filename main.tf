973fe9b80adedbae8d363d65303223ffa1ff3901
root <root@PradeepKM.022woc3sggte1kifbuvykrrxeh.rx.internal.cloudapp.net>
2022-12-30 18:07:46 +0000

    uytr

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ depends_on          = [azurerm_resource_group.rg]
   }
 }
 resource "azurerm_container_registry" "acr" {
-  name     = "pradeep"
+  name     = "pradeep_registry"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   sku                      = "Basic"
